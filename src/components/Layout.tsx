2ad1ef7f1d91a626edf2e5493d3135b0372f17db
jjjjonathan <hornathanjon@gmail.com>
2022-05-10 19:51:23 -0700

    Setup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import GlobalStyle from '../styles/global';
 import { Helmet } from 'react-helmet';
+import GlobalStyle from '../styles/global';
 import '@picocss/pico/css/pico.min.css';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => {
-  return (
-    <>
-      <Helmet>
-        <title>Jonathan Landon</title>
-      </Helmet>
-      <GlobalStyle />
-      <main>{children}</main>
-    </>
-  );
-};
+const Layout = ({ children }: Props) => (
+  <>
+    <Helmet>
+      <title>Jonathan Landon</title>
+    </Helmet>
+    <GlobalStyle />
+    <main>{children}</main>
+  </>
+);
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <h1>Hello World</h1>
-      <button>yeet</button>
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello World</h1>
+  </Layout>
+);
 
 export default IndexPage;
